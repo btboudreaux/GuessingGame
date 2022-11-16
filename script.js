80afc104bc68692fb58115a0dbb18dc90b473384
Brad Boudreaux <brad.boudreaux@gmail.com>
2022-11-15 20:36:03 -0600

    Update

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ const btnOptions = document.querySelectorAll(".btn-num-options");
 const message = document.querySelector(".message");
 const numInput = document.querySelector(".num-input");
 const btnCheck = document.querySelector(".btn-check");
+const currentScore = document.querySelector(".currentScore");
 
 let secretNum;
-let range;
+let rangeNum;
+let score = 20;
 let highScore50 = 0;
 let highScore100 = 0;
 let highScore1000 = 0;
@@ -20,8 +22,27 @@ function btnOption(num) {
   return getRandomNum(num);
 }
 
+function getGuess() {
+  return Number(numInput.value);
+}
+
+function checkLose() {
+  subtractScore();
+  if (score === 0) {
+    btnCheck.disabled = true;
+    message.textContent = "You have lost! Whomp! Whomp!";
+    document.querySelector("body").style.backgroundColor = "rgb(105, 47, 47)";
+  }
+}
+
+function subtractScore() {
+  score--;
+  currentScore.textContent = score;
+}
+
 btnOptions.forEach((button) => {
   button.onclick = function () {
+    rangeNum = Number(button.dataset.num);
     secretNum = btnOption(Number(button.dataset.num));
     message.textContent = `Choose a number between 1 and ${button.dataset.num}`;
     btnOptions.forEach((buttonClicked) => {
@@ -40,13 +61,19 @@ btnOptions.forEach((button) => {
 btnCheck.disabled = true;
 
 btnCheck.onclick = function () {
-  if (!numInput.value || numInput.value < 1) {
+  const guess = getGuess();
+  if (!guess || guess < 1 || guess > rangeNum) {
     message.textContent = "Please enter a valid number!";
+  } else if (guess !== secretNum) {
+    message.textContent =
+      guess > secretNum
+        ? `📈 ${guess} is Too High 📈`
+        : `📉 ${guess} is Too Low 📉`;
+    checkLose();
+  } else if (guess === secretNum) {
+    message.textContent = "You Won!";
   }
-  console.log(numInput.value);
 };
 
 //For displaying number
 // secretNumBox.style.width = "220px";
-
-// console.log(secretNum);
